18c7ce072db28794478a98d58635c8b5a24a21f3
bojanche <Boj@n@25122015>
2020-11-28 07:38:44 +0100

    commit 1

---
+++ checkpass.py
@@ -0,0 +1,34 @@
+import requests
+import hashlib
+import sys
+
+
+def request_api_data(hash_to_check):
+    address = 'https://api.pwnedpasswords.com/range/' + hash_to_check
+    res = requests.get(address)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}')
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, counted in hashes:
+        if h == hash_to_check:
+            return counted
+    return 0
+
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+
+password = input('Please enter password to check: ')
+count = pwned_api_check(password)
+if count:
+    print(f'{password} was found {count} times... you should change your password!')
+else:
+    print(f'{password} was NOT found.')
